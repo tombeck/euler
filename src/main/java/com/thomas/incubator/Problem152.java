2f87d189fe48bd8e6b19e4ee26042b78d0db330b
Thomas Beckmann <thomas.beckmann@freiheit.com>
2010-06-10 18:36:11 +0200

    tried again problem 152 but failed

--- src/main/java/com/thomas/incubator/Problem152.java
+++ src/main/java/com/thomas/incubator/Problem152.java
@@ -23,14 +23,9 @@
  */
 package com.thomas.incubator;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import com.thomas.util.Euler;
-import com.thomas.util.PrimeUtils;
 import com.thomas.util.Euler.Problem;
+import com.thomas.util.rational.BigRational;
 
 /**
  * TODO Type documentation
@@ -40,9 +35,23 @@ import com.thomas.util.Euler.Problem;
  */
 class Problem152 implements Problem {
 
+    static BigRational HALF = new BigRational(1, 2);
+    
     /**
      * TODO Method documentation
      * 
+     *  2^2 = 2^2 
+     *  3^3 = 3^2 : 2^2 * 3^2
+     *  4^4 = 2^4 : 2^4 * 3^2
+     *  5^5 = 5^5 : 2^4 * 3^2 * 5^2
+     *  6^6 = 2^2 * 3^2 : 2^4 * 3^2 * 5^2
+     *  7^7 = 7^7 : 2^4 * 3^2 * 5^2 * 7^2
+     *  8^8 = 2^6 : 2^6 * 3^2 * 5^2 * 7^2
+     *  9^2 = 3^4 : 2^6 * 3^4 * 5^2 * 7^2
+     * 10^2 = 2^2 * 5^2 : 2^6 * 3^4 * 5^2 * 7^2
+     * 11^2 = 11^2 : 2^6 * 3^4 * 5^2 * 7^2 * 11^2
+     * 12^2 = 2^4 * 3^2 : 2^6 * 3^4 * 5^2 * 7^2 * 11^2
+     * 13^2
      * @return
      * @see com.thomas.util.Euler.Problem#solve()
      * @author Thomas
@@ -51,102 +60,30 @@ class Problem152 implements Problem {
     @Override
     public Object solve() {
 
-        final int n = 45;
+        final BigRational[][] fractions = new BigRational[45 - 1][2];
         
-        final BigInteger den = getDenominator(n);
-        final BigInteger[] nom = new BigInteger[n + 1];
-        final BigInteger[] sum = new BigInteger[n + 1];
-        
-        sum[n] = nom[n] = den.divide(BigInteger.valueOf(n * n));
-        for (int i = n - 1; i >= 2; --i) {
-            nom[i] = den.divide(BigInteger.valueOf(i * i));
-            sum[i] = nom[i].add(sum[i + 1]);
+        for (int i = 0; i < fractions.length; ++i) {
+            fractions[i][0] = new BigRational(1, (i + 2) * (i + 2));
         }
-
-        return getCount(sum, nom, den.divide(BigInteger.valueOf(2)), BigInteger.ZERO, 2, n);
+        fractions[fractions.length - 1][1] = fractions[fractions.length - 1][0];
+        for (int i = fractions.length - 1; i-- > 0; ) {
+            fractions[i][1] = fractions[i + 1][1].add(fractions[i][0]);
+        }
+        
+        return count(fractions, 0, HALF);
     }
 
-    private int getCount(BigInteger[] sum, BigInteger[] nom, BigInteger maxNom, BigInteger curNom, int i, int n) {
-        
-        int count = 0;
-        
-        int min = min(nom, maxNom, i, n, curNom);
-        int max = max(sum, maxNom, i, n, curNom);
-        
-        for (int j = min; j <= Math.min(max, n); ++j) {
-//            if (curNom.add(sum[j]).compareTo(maxNom) < 0) break;
-            
-//            final BigInteger nextNom = curNom.add(nom[j]);
-//            final int c = nextNom.compareTo(maxNom);
-//            
-//            if (c < 0) count += getCount(sum, nom, maxNom, nextNom, j + 1, n);
-//            else if (c == 0) count += 1;
-//            else break;
-            count += getCount(sum, nom, maxNom, curNom.add(nom[j]), j + 1, n);
-        }
-        
-        return count;
-    }
-    
-    private int min(BigInteger[] nom, BigInteger maxNom, int first, int last, BigInteger curNom) {
-    
-        while (first < last) {
-            int mid = (first + last) / 2;
+    private int count(BigRational[][] fractions, int i, BigRational cur) {
 
-            int c = curNom.add(nom[mid]).compareTo(maxNom);
-            if (c > 0) first = mid + 1;
-            else last = mid + 1;
-        }
+        final int c = cur.compareTo(BigRational.ZERO);
         
-        return first;
-    }
-    
-    private int max(BigInteger[] sum, BigInteger maxNom, int first, int last, BigInteger curNom) {
+        if (c < 0) return 0;
+        if (c == 0) return 1;
+        if (i >= fractions.length) return 0;
+        if (cur.compareTo(fractions[i][1]) > 0) return 0;
         
-        while (first < last) {
-            int mid = (first + 1 + last) / 2;
-
-            int c = curNom.add(sum[mid]).compareTo(maxNom);
-            if (c < 0) last = mid - 1;
-            else first = mid;
-        }
         
-        return last;
-    }
-    
-    private BigInteger getDenominator(int n) {
-        
-        final Map<Long, Integer> map = new HashMap<Long, Integer>();
-        
-        for (int i = 2; i <= n; ++i) {
-            
-            long prev = 1;
-            int count = 0;
-            
-            for (long p : PrimeUtils.getPrimeFactors(i * i)) {
-                if (p == prev) ++count;
-                else {
-                    
-                    final Integer c = map.get(prev);
-                    
-                    if (c == null) map.put(prev, count);
-                    else map.put(prev, Math.max(c, count));
-                    count = 1;
-                }
-                prev = p;
-            }
-            Integer c = map.get(prev);
-            if (c == null) map.put(prev, count);
-            else map.put(prev, Math.max(c, count));
-        }
-        
-        BigInteger den = BigInteger.ONE;
-        
-        for (Entry<Long, Integer> entry : map.entrySet()) {
-            den = den.multiply(BigInteger.valueOf(entry.getKey()).pow(entry.getValue()));
-        }
-        
-        return den;
+        return count(fractions, i + 1, cur) + count(fractions, i + 1, cur.subtract(fractions[i][0]));
     }
     
     /**
