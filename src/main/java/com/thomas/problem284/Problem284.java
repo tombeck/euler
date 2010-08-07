46225fd4f3a2f14209734a4400c56a2542bbb543
Thomas Beckmann <thomas.beckmann@freiheit.com>
2010-08-07 20:46:52 +0200

    solved problem 284

--- src/main/java/com/thomas/problem284/Problem284.java
+++ src/main/java/com/thomas/problem284/Problem284.java
@@ -21,9 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.thomas.incubator;
-
-import java.util.Arrays;
+package com.thomas.problem284;
 
 import com.thomas.util.Euler;
 import com.thomas.util.Euler.Problem;
@@ -34,12 +32,12 @@ import com.thomas.util.Euler.Problem;
  * @author Thomas
  * @since 31.03.2010
  */
-class Problem284 implements Problem {
+public class Problem284 implements Problem {
 
     private static final char[] BASE14 = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd'};
-    
+
     /**
-     * TODO Method documentation
+     * TODO Method documentation 5a411d7b
      * 
      * @return
      * @throws Exception
@@ -48,36 +46,58 @@ class Problem284 implements Problem {
      * @since 31.03.2010
      */
     @Override
-    public Object solve() {
+    public String solve() {
 
-        for (int i = 0; i < 14; ++i) {
-            if (i == (i * i) % 14) System.out.println(i);
+        final int max = 10000;
+        
+        final int[] x = new int[max];
+        final int[] x2 = new int[2 * max];
+        final int[] y = new int[max];
+        final int[] y2 = new int[2 * max];
+        
+        int sum = 1 + 8 + 7;
+        
+        x[0] = 8;
+        y[0] = 7;
+        for (int n = 1, sumx = 8, sumy = 7; n < max; ++n) {
+            add(x, x2, n - 1);
+            add(y, y2, n - 1);
+            sumx += x[n] = (14 - x2[n]) % 14;
+            sumy += y[n] = y2[n];
+            if (x[n] > 0) sum += sumx;
+            if (y[n] > 0) sum += sumy;
         }
-        return null;
+
+        return toBase14(sum);
     }
 
-    private String toBase14(int n) {
+    private void add(int[] x, int[] x2, int n) {
+        
+        final int d2 = x[n] << 1;
+        
+        int carry = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            x2[n + i] = (carry = d2 * x[i] + x2[n + i] + carry) % 14;
+            carry /= 14;
+        }
+        
+        int sqr = x[n] * x[n] + carry;
+        x2[2 * n] = sqr % 14;
+        x2[2 * n + 1] = sqr / 14;
+    }
     
-        StringBuilder builder = new StringBuilder();
+    private String toBase14(long n) {
+
+        final StringBuilder builder = new StringBuilder();
         
         for (; n > 0; n /= 14) {
-            builder.append(BASE14[n % 14]);
+            builder.append(BASE14[(int)(n % 14)]);
         }
         
         return builder.reverse().toString();
     }
-    
-    private int toBase10(String s) {
-    
-        int n = 0;
-        
-        for (char c : s.toCharArray()) {
-            n = n * 14 + Arrays.binarySearch(BASE14, c);
-        }
-        
-        return n;
-    }
-    
+
     /**
      * TODO Method documentation
      * 
--- src/test/java/com/thomas/TestAllProblems.java
+++ src/test/java/com/thomas/TestAllProblems.java
@@ -200,6 +200,7 @@ import com.thomas.problem267.Problem267;
 import com.thomas.problem271.Problem271;
 import com.thomas.problem277.Problem277;
 import com.thomas.problem281.Problem281;
+import com.thomas.problem284.Problem284;
 import com.thomas.problem293.Problem293;
 import com.thomas.problem2xx.problem20x.Problem203;
 import com.thomas.problem2xx.problem20x.Problem204;
@@ -505,6 +506,8 @@ public class TestAllProblems {
                 { new Problem277(), "1125977393124310" },
 
                 { new Problem281(), "1485776387445623" },
+
+                { new Problem284(), "5a411d7b" },
 
                 { new Problem293(), "2209" },
 
