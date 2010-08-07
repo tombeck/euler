bf2734544be2274560702266212abf020cc31d79
Thomas Beckmann <thomas.beckmann@freiheit.com>
2010-08-07 20:47:32 +0200

    tried problem 266 but failed

--- src/main/java/com/thomas/incubator/Problem266.java
+++ src/main/java/com/thomas/incubator/Problem266.java
@@ -24,9 +24,11 @@
 package com.thomas.incubator;
 
 import java.math.BigInteger;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.thomas.util.Euler;
+import com.thomas.util.NumberUtils;
 import com.thomas.util.PrimeUtils;
 import com.thomas.util.Euler.Problem;
 
@@ -58,7 +60,23 @@ class Problem266 implements Problem {
      * 2323212132197291910298742221654621170
      * 2323218028627439520241663855180334010
      * 2323218285027841537300389111299582970
+     * 2323218884340250580009092219729352310
+     * 2323218884340250580009092219729352310
      * 2323218898901780889985862615189951490
+     * 2323218908561298733511111474936082035
+     * 2323218921858619707286894168125203145
+     * 2323218921876475461052305699396574695
+     * 2323218933443207472763771566724863486
+     * 2323218934233298386205079941055316570
+     * 2323218944614584322270018016078216835
+     * 2323218945249486638413101515623452918
+     * 2323218947021562122734557024235572863
+     * 2323218947508044464337920434523462185
+     * 2323218949910263626509980668701362170
+     * 2323218950435470564826515588366729785
+     * 2323218950596752653151031424716658730
+     * 
+     * 2323218950681478446587180516177954548
      * 
      * @return
      * @see com.thomas.util.Euler.Problem#solve()
@@ -67,47 +85,57 @@ class Problem266 implements Problem {
      * @since 14.03.2010
      */
     @Override
-    public Object solve() throws InterruptedException {
+    public Object solve() {
 
-        for (int p : PrimeUtils.primes(190)) {
-            int q = (int)Math.sqrt(p);
-            System.out.println(p + ", " + q + ", " + (q * q) + ", " + ((q + 1) * (q + 1)));
-        }
-//        final List<BigInteger> primes = new ArrayList<BigInteger>();
-//        
-//        BigInteger b = ONE;
-//
-//        for (int p : Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53)) {
-//            final BigInteger prime = BigInteger.valueOf(p);
-//            b = b.multiply(prime);
-//            primes.add(prime);
-//        }
-//
-//        return PrimeUtils.getPrimeFactors(psr(primes, 0, ONE, b, 0, ONE).longValue());
+        final int[] primes = NumberUtils.toIntArray(PrimeUtils.primes(190));
+        //ArrayUtils.reverse(primes, 0, primes.length);
+        BigInteger p = BigInteger.ONE;
         
-        return null;
+        for (int a : primes) {
+            p = p.multiply(BigInteger.valueOf(a));
+        }
+        BigInteger max = sqrt(p);
+        BigInteger[][] a = new BigInteger[primes.length][2];
+        
+        for (int i = 0; i < a.length; ++i) {
+            a[i][0] = BigInteger.valueOf(primes[i]);
+            a[i][1] = (p = p.divide(a[i][0]));
+        }
+
+//        new BigInteger("");
+//        return find(a, 0, BigInteger.ONE, BigInteger.ONE, p);
+        return find(a, 0, BigInteger.ONE, BigInteger.ONE, max, new HashSet<Integer>()).mod(BigInteger.TEN.pow(16));
     }
 
-    private BigInteger psr(List<BigInteger> primes, int i, BigInteger l, BigInteger g, int j, BigInteger max) throws InterruptedException {
+    private BigInteger find(BigInteger[][] a, int i, BigInteger cur, BigInteger best, BigInteger max, Set<Integer> res) {
         
-//        if (i <= 20) System.out.println(i);
-//        if (i == 30) Thread.sleep(1);
+        if (i == a.length) return best;
         
-        if (l.compareTo(g) >= 0) return max;
-        if (i == primes.size()) {
-            if (l.compareTo(max) > 0) {
-//                System.out.println(l);
-                return l;
+        BigInteger m = cur.multiply(a[i][0]);
+        
+        if (m.compareTo(max) <= 0) {
+            if (m.compareTo(best) > 0) {
+                best = m;
+                System.out.println(best);
             }
-            return max;
+            best = find(a, i + 1, m, best, max, res);
+            if (cur.multiply(a[i][1]).compareTo(best) > 0) {
+                best = find(a, i + 1, cur, best, max, res);
+            }
         }
         
-        final BigInteger prime = primes.get(i++);
+        return best;
+    }
+    
+    private BigInteger sqrt(BigInteger n) {
         
-        max = psr(primes, i, l.multiply(prime), g.divide(prime), j + 1, max);
-        max = psr(primes, i, l, g, j, max);
+        BigInteger[] x = {n, n.add(BigInteger.ONE).divide(BigInteger.valueOf(2))};
         
-        return max;
+        while (!x[0].equals(x[1])) {
+            x = new BigInteger[] {x[1], n.divide(x[1]).add(x[1]).divide(BigInteger.valueOf(2))};
+        }
+
+        return x[0];
     }
     
     /**
