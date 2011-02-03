0808dcfbe0d1dbce5ecebcb3b882bdfc4d8b147c
Thomas Beckmann <thomas.beckmann@freiheit.com>
2011-02-03 19:03:36 +0100

    tried problem 152 but failed

--- src/main/java/com/thomas/incubator/Problem152.java
+++ src/main/java/com/thomas/incubator/Problem152.java
@@ -23,6 +23,8 @@
  */
 package com.thomas.incubator;
 
+import static java.lang.Math.exp;
+
 import java.math.BigInteger;
 
 import com.thomas.util.Euler;
@@ -36,101 +38,20 @@ import com.thomas.util.Euler.Problem;
  */
 class Problem152 implements Problem {
 
-    static class BigRational implements Comparable<BigRational> {
-
-        private final BigInteger num;
-        private final BigInteger den;
-        
-        /**
-         * @param num
-         * @param den
-         */
-        public BigRational(BigInteger num, BigInteger den) {
-
-            this.num = num;
-            this.den = den;
-        }
-
-        /**
-         * @param num
-         * @param den
-         */
-        public BigRational(long num, long den) {
-
-            this(BigInteger.valueOf(num), BigInteger.valueOf(den));
-        }
-
-        /**
-         * @param num
-         * @param den
-         */
-        public BigRational(long num) {
-
-            this(BigInteger.valueOf(num), BigInteger.ONE);
-        }
-
-        public BigRational add(BigRational other) {
-        
-            return new BigRational(this.num.multiply(other.den).add(other.num.multiply(this.den)), this.den.multiply(other.den));
-        }
-
-        public BigRational subtract(BigRational other) {
-            
-            return new BigRational(this.num.multiply(other.den).subtract(other.num.multiply(this.den)), this.den.multiply(other.den));
-        }
-
-        public int signum() {
-            
-            return this.num.signum();
-        }
-        
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public int compareTo(BigRational other) {
-            
-            return this.num.multiply(other.den).compareTo(other.num.multiply(this.den));
-        }
-
-        public boolean less(BigRational other) {
-            
-            return this.num.multiply(other.den).compareTo(other.num.multiply(this.den)) < 0;
-        }
-
-        @Override
-        public String toString() {
-        
-            return this.num + (BigInteger.ONE.equals(this.den) ? "" : "/" + this.den);
-        }
-
-    }
-    
-    static final BigRational ZERO = new BigRational(0);
-    static final BigRational ONE = new BigRational(1);
-    static BigRational HALF = new BigRational(1, 2);
-    
     /**
      * TODO Method documentation
-     * 
-     *  2^2 = 2^2 
-     *  3^3 = 3^2 : 2^2 * 3^2
-     *  4^4 = 2^4 : 2^4 * 3^2
-     *  5^5 = 5^5 : 2^4 * 3^2 * 5^2
-     *  6^6 = 2^2 * 3^2 : 2^4 * 3^2 * 5^2
-     *  7^7 = 7^7 : 2^4 * 3^2 * 5^2 * 7^2
-     *  8^8 = 2^6 : 2^6 * 3^2 * 5^2 * 7^2
-     *  9^2 = 3^4 : 2^6 * 3^4 * 5^2 * 7^2
-     * 10^2 = 2^2 * 5^2 : 2^6 * 3^4 * 5^2 * 7^2
-     * 11^2 = 11^2 : 2^6 * 3^4 * 5^2 * 7^2 * 11^2
-     * 12^2 = 2^4 * 3^2 : 2^6 * 3^4 * 5^2 * 7^2 * 11^2
-     * 13^2
      * 
      * 35 > 1 in    4137 ms
      * 36 > 1 in    7813 ms
      * 37 > 1 in   14481 ms
      * 38 > 1 in   26867 ms
-     * 45 > 3 in 3914081 ms
+     * 40 > 1 in   16319 ms
+     * 41 > 1 in   30301 ms
+     * 42 > 1 in   58079 ms
+     * 43 > 1 in  125921 ms
+     * 44 > 1 in  262897 ms
+     * 45 > 3 in  436402 ms
+     * 46 > 3 in 1898423 ms
      * 
      * a/b + x/y = 1/2 <=> 2(ay + xb) = yb
      * 
@@ -140,8 +61,14 @@ class Problem152 implements Problem {
      * 
      * 1/a + 1/b + 1/c = (bc + ac + ab) / abc
      * 
-     * 
-     * 
+     * nicht 32
+     * nicht 64
+     * wenn 16, dann auch einer von (48 oder 80)
+     * wenn 48, dann auch einer von (16 oder 80)
+     * wenn 80, dann auch einer von (16 oder 48)
+     * wenn 25, dann auch einer von (50 oder 75)
+     * wenn 50, dann auch 25
+     * wenn 75, dann auch 25
      * 
      * @return
      * @see com.thomas.util.Euler.Problem#solve()
@@ -151,48 +78,36 @@ class Problem152 implements Problem {
     @Override
     public Object solve() {
 
-        final BigInteger[][] fractions = new BigInteger[35 - 1][2];
+        final int max = 80;
+        final double[][] f = new double[max - 1][2];
         
-        BigInteger sum = BigInteger.ZERO;
-        BigInteger lcm = BigInteger.valueOf(2);
+        double prod = 1;
         
-        for (int i = fractions.length; i-- > 0; ) {
-            lcm = lcm(lcm, BigInteger.valueOf((i + 2) * (i + 2)));
+        for (int i = f.length; i-- > 0; ) {
+            f[i][0] = exp(1.0 / ((i + 2) * (i + 2)));
+            System.out.println((i + 2) + " " + f[i][0]);
+            prod *= f[i][0];
+            f[i][1] = prod;
         }
-        for (int i = fractions.length; i-- > 0; ) {
-            fractions[i][0] = lcm.divide(BigInteger.valueOf((i + 2) * (i + 2)));
-            sum = sum.add(fractions[i][0]);
-            fractions[i][1] = sum;
-            if (i + 2 == 25) continue;
-            if (i + 2 == 32) continue;
-            if (i + 2 == 16) continue;
-            System.out.println((i + 2) + ":" + fractions[i][0].mod(BigInteger.TEN.pow(4)));
-        }
-        return count(fractions, 0, lcm.divide(BigInteger.valueOf(2)), 0);
+        System.out.println(exp(0.5));
+        
+        return count(f, 0, 1.0, exp(0.5));
     }
-
+    
     private BigInteger lcm(BigInteger a, BigInteger b) {
     
         return a.divide(a.gcd(b)).multiply(b);
     }
     
-    private int count(BigInteger[][] fractions, int i, BigInteger cur, int d) {
+    private int count(double[][] fractions, int i, double cur, double target) {
 
-        ++d;
-        
-        //if (d < 7) System.out.println(d + ", " + i);
-        
-        if (cur.signum() == 0) return 1;
+        if (cur == target) return 1;
         
         int count = 0;
         
-        for (; i < fractions.length && cur.compareTo(fractions[i][0]) < 0; ++i) {
-        }
-        for (; i < fractions.length && cur.compareTo(fractions[i][1]) <= 0; ++i) {
-            if (i + 2 == 25) continue;
-            if (i + 2 == 32) continue;
-            if (i + 2 == 16) continue;
-            count += count(fractions, i + 1, cur.subtract(fractions[i][0]), d);
+        for (; i < fractions.length && cur * fractions[i][0] > target; ++i);
+        for (; i < fractions.length && cur * fractions[i][1] >= target; ++i) {
+            count += count(fractions, i + 1, cur * fractions[i][0], target);
         }
 
         return count;
